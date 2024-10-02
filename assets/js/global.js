aa28fb90e2a3995aeb5bf1dae75ca6bbcea729c2
Naser-Alzaghari <naseralzaghari88@gmail.com>
2024-10-02 12:02:28 +0300

    Upload

---
+++ assets/js/global.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// Add event on multiple elements
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const elem of elements) elem.addEventListener(eventType, callback);
+};
+
+// Toggle search box in mobile device || small screen
+const searchBox = document.querySelector("[search-box]");
+const searchTogglers = document.querySelectorAll("[search-toggler]");
+
+addEventOnElements(searchTogglers, "click", function () {
+  searchBox.classList.toggle("active");
+});
+
+// store movieID in `LocalStorage` when you click any card
+const getMovieDetail = function (movieId) {
+  window.localStorage.setItem("movieId", String(movieId));
+};
+
+const getMovieList = function (urlParam, genreName) {
+  window.localStorage.setItem("urlParam", urlParam);
+  window.localStorage.setItem("genreName", genreName);
+};
+
+let favorite_Arr = JSON.parse(localStorage.getItem("array"));
+function favorite_togle(id){
+  let b = document.querySelector("#favorite-button-id"+id);
+  b.classList.toggle("favorite-button");
+  if(b.getAttribute("favotite") == "false"){
+    favorite_Arr.push(id);
+    b.setAttribute("favotite","true");
+  } else {
+    favorite_Arr.splice(favorite_Arr.indexOf(id),1);
+    console.log(favorite_Arr.indexOf(id)+1);
+    
+    b.setAttribute("favotite","false");
+
+  }
+console.log(favorite_Arr);
+let temp = JSON.stringify(favorite_Arr);
+localStorage.setItem("array",temp);
+let array = JSON.parse(localStorage.getItem("array"));
+console.log(array);
+
+}
+
+
+// export {favorite_Arr}
---
+++ assets/js/movie-card.js
@@ -0,0 +1,87 @@
+"use strict";
+
+import { imageBaseURL } from "./api.js";
+
+// movie card
+
+export function createMovieCard(movie) {
+  const { poster_path, title, vote_average, release_date, id } = movie;
+
+  const card = document.createElement("div");
+  card.classList.add("movie-card");
+
+
+
+
+
+  let favorite = "false";
+
+  if (favorite_Arr.includes(id)){
+    let favorite = "ture";
+  }
+   
+
+  card.innerHTML = `
+    <figure class="poster-box card-banner" style="display: relative;">
+      <img src="${imageBaseURL}w342${poster_path}" alt="${title}" class="img-cover" loading="lazy">
+    </figure>
+    
+    <h4 class="title">${title}</h4>
+    <button id="favorite-button-id${id}" class="favorite" href="" style="z-index=1000; display: abslute;" onclick="favorite_togle(${id});" favotite="${favorite}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+</svg></button>
+    <div class="meta-list">
+      <div class="meta-item">
+        <img src="./assets/images/star.png" width="20" height="20" loading="lazy" alt="rating">
+    
+        <span class="span">${vote_average.toFixed(1)}</span>
+      </div>
+    
+      <div class="card-badge">${release_date.split("-")[0]}</div>
+    </div>
+    
+        <a href="./detail.html" class="card-btn" title="${title}" onclick="getMovieDetail(${id})"></a>
+  `;
+
+  
+  // if (favorite_Arr.includes(id))
+  //   document.getElementById("favorite-button-id"+id).setAttribute("favotite","ture");
+  // else
+  //   document.getElementById("favorite-button-id"+id).setAttribute("favotite","false");
+
+   
+  return card;
+}
+
+export function createMovieCardfavorite(movie) {
+  const { poster_path, title, vote_average, release_date, id } = movie;
+
+  const card = document.createElement("div");
+  card.classList.add("movie-card");
+
+  card.innerHTML = `
+    <figure class="poster-box card-banner" style="display: relative;">
+      <img src="${imageBaseURL}w342${poster_path}" alt="${title}" class="img-cover" loading="lazy">
+    </figure>
+    
+    <h4 class="title">${title}</h4>
+    <button id="favorite-button-id${id}" class="favorite" href="" style="z-index=100; display: abslute;" onclick="favorite_togle(${id});" favotite="false"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+</svg></button>
+    <div class="meta-list">
+      <div class="meta-item">
+        <img src="./assets/images/star.png" width="20" height="20" loading="lazy" alt="rating">
+    
+        <span class="span">${vote_average.toFixed(1)}</span>
+      </div>
+    
+      <div class="card-badge">${release_date.split("-")[0]}</div>
+    </div>
+    
+    <a href="./detail.html" class="card-btn" title="${title}" onclick="getMovieDetail(${id})"></a>
+  `;
+
+  return card;
+}
+
+
